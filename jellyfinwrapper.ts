1a06b28023e9348bb140046c97d6cdc39df5e1aa
flloschy <floschy.email@gmail.com>
2025-04-21 03:07:09 +0200

    formatting

--- jellyfinwrapper.ts
+++ jellyfinwrapper.ts
@@ -163,20 +163,20 @@ export class Jellyfin {
       let smallImageKey = playback.paused ? "pause" : undefined;
       if (!smallImageKey) {
         for (const id of playback.artistIds) {
-            const url = this.url + `Items/${id}/Images/Primary`;
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    Authorization: this.header!,
-                    "Content-Type": "application/json",
-                },
-            });
-            if (response.ok) {
-                smallImageKey = url;
-                break;
-            }
+          const url = this.url + `Items/${id}/Images/Primary`;
+          const response = await fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: this.header!,
+              "Content-Type": "application/json",
+            },
+          });
+          if (response.ok) {
+            smallImageKey = url;
+            break;
+          }
         }
-    }
+      }
 
       let largeImageKey;
       for (const id of [playback.id, playback.parentId]) {
